da5a5cd13b39e3b7ca31e030c1b751f0ddaea004
Yahya Abad <yabad@c1r4p10.1337.ma>
2024-05-27 18:38:11 +0100

    defining a Link Model

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Link(models.Model):
+	user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+	url = models.URLField(unique=True)
+	short_url = models.URLField(unique=True)
+	title = models.CharField(max_length=255)
+	description = models.TextField(blank=True, null=True)
+	image = models.URLField(blank=True, null=True)
+	views_count = models.PositiveIntegerField(default=0)
+	created_at = models.DateTimeField(auto_now_add=True)
+	destroyed_at = models.DateTimeField()
+
+	def __str__(self):
+		return self.short_url
