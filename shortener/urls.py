805b298f1d72049c217d55d1eea8d8c2f498d497
Yahya Abad <yabad@c1r4p10.1337.ma>
2024-05-28 12:19:58 +0100

    adding a redirect view and a redirect route to the link.url

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, redirect_view
 
 urlpatterns = [
 	path('', index, name='index'),
+	path('view/<str:short_code>/', redirect_view, name='redirect'),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import LinkForm
 from .models import Link
+from .base62 import base62_decode
 
 def index(request):
 	if request.method == 'POST':
@@ -12,3 +13,10 @@ def index(request):
 		form = LinkForm()
 	links = Link.objects.all()
 	return render(request, 'index.html', {'form': form, 'links': links})
+
+def redirect_view(request, short_code):
+	link_id = base62_decode(short_code)
+	link = get_object_or_404(Link, pk=link_id)
+	link.views_count += 1
+	link.save()
+	return redirect(link.url)
